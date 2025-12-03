9812b5fa40a6454f1b6b89c0c5e5613c2d08ed04
miaomiaowawa-dot <miaomiaowawa@gmail.com>
2025-12-03 22:55:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -177,7 +177,12 @@ def about():
     """关于系统页面"""
     return render_template('about.html')
 
+@app.route('/health')
+def health():
+    return "OK", 200
+
 if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0', port=5000)
     port = int(os.environ.get('PORT', 5000))
+
     app.run(host='0.0.0.0', port=port, debug=False)
